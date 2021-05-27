8e0affc4c3c87cb8f9698437d4949dfbfc79f6a9
nickfarrow <nick@nickfarrow.com>
2021-05-27 13:54:58 +1000

    Route bitcoind payments via lnd

--- config.py
+++ config.py
@@ -4,8 +4,8 @@ import os
 # This should point to your bitcoin/lnd node,
 # with the correct RPC port as set in your config.
 # Connecting through local host as  i'm running SatSale on my node
-host = os.getenv("BITCOIND_HOST")
-rpcport = os.getenv("BITCOIND_PORT")
+host = os.getenv("LND_HOST")
+rpcport = os.getenv("LND_PORT")
 
 # From ~/.bitcoin/bitcoin.conf
 username = os.getenv("BITCOIND_USER")
@@ -25,7 +25,7 @@ api_key_path = "SatSale_API_key"
 # SSH tunnel to node
 # Make sure this command works `ssh HOST@IP -q -N -L 8332:localhost:8332`
 # Use host = "127.0.0.1" and you will be able to see your node on 8332
-tunnel_host = None # "HOST@IP"
+tunnel_host = None #"HOST@IP"
 
 # or tor hidden service for RPC (see docs for how to set up), need onion:
 tor_bitcoinrpc_host = None # e.g. "http://if...dwr.onion"
@@ -49,14 +49,14 @@ connection_attempts = 3
 redirect = "https://github.com/nickfarrow/satsale"
 
 # Payment method
-pay_method = "bitcoind"
+#pay_method = "bitcoind"
 # Switch payment_method to lnd if you want to use lightning payments instead. And uncomment lnd_dir.
-#pay_method = "lnd"
+pay_method = "lnd"
 # lnd_dir is only needed if you want to copy macaroon and TLS cert locally
-#lnd_dir = "~/.lnd/"
-#lnd_rpcport = "10009"
-#lnd_macaroon = "invoice.macaroon"
-#lnd_cert = "tls.cert"
+lnd_dir = "~/.lnd/"
+lnd_rpcport = os.getenv("LND_PORT")
+lnd_macaroon = os.getenv("MACAROON_DIR")
+lnd_cert = os.getenv("TLS_FILE")
 
 # DO NOT CHANGE THIS TO TRUE UNLESS YOU WANT ALL PAYMENTS TO AUTOMATICALLY
 # BE CONSIDERED AS PAID.
